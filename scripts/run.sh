a3a9d08f8f4a08502a15d1053a9cfbccebeb8fb7
modolee <modolee.si@gmail.com>
2024-06-30 22:04:24 +0900

    fix: 스크립트 다시 추가

---
+++ scripts/run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# main 브랜치로 이동
+git switch main
+
+# 최신 소스 코드를 가져옴
+git pull
+
+# 의존성 설치
+yarn --frozen-lockfile
+
+# 빌드
+yarn build
+
+# PM2로 실행 중인 서버 중지 및 삭제
+pm2 delete sparta-nest-cicd
+
+# 서버를 PM2로 실행
+pm2 --name sparta-nest-cicd start dist/main.js
+
+# PM2 설정 저장 (선택사항, startup 설정을 해놨다면)
+pm2 save
